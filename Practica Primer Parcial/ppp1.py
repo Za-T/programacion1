e07af8a9058a2b35f5e7d4bccbb5cb929b012fb4
Za-T <zazatunik@gmail.com>
2025-05-16 16:11:46 -0300

    ppp1

--- Practica Primer Parcial/ppp1.py
+++ Practica Primer Parcial/ppp1.py
@@ -1,11 +1,13 @@
 def verificar_tesoro (mapa:list, x:int, y:int) ->int:
 
     if mapa [x][y] == 1:
-        devolver = 0
+        distancia = 0
+
     else:
-        devolver = (x - 1) + (y - 3)
-    
-    return devolver
+        distancia = (x - 1) + (y - 3)
+        distancia = distancia * (-1)
+
+    return distancia
 
 
 def radar_del_tesoro ():
@@ -25,13 +27,12 @@ def radar_del_tesoro ():
         x = int (input ("Ingrese coordenada de fila (0-4): "))
 
         if x < 0 or x > 4:
-
-            repetir = input("Error. Ingrese coordenada de fila (0-4): ")
+            x = int(input("Error. Ingrese coordenada de fila (0-4): "))
 
         y = int (input ("Ingrese coordenada de columna (0-4): "))
 
         if y < 0 or y > 4:
-            repetir = input("Error. Ingrese coordenada de columna (0-4): ")
+            y = int(input("Error. Ingrese coordenada de columna (0-4): "))
 
 
         resultado = verificar_tesoro(mapa, x, y)
@@ -48,7 +49,7 @@ def radar_del_tesoro ():
 
             repetir = input("Quiere repetir (s/n)? ")
 
-            '''while repetir != "s" or repetir != "n":
-                repetir = input("Error. Quiere repetir (s/n)? ")'''
+            if repetir != "s" and repetir != "n":
+                repetir = input("Error. Quiere repetir (s/n)? ")
 
 radar_del_tesoro ()
